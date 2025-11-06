4339d4e65e55368374ec7a7c4135d010ca8b3248
duinodu <472365351duino@gmail.com>
2025-11-07 01:36:14 +0800

    update flash_attn.sh

--- src/install_x/install/flash_attn.sh
+++ src/install_x/install/flash_attn.sh
@@ -33,6 +33,12 @@ fi
 if [ -d build ];then
     rm -rf build
 fi
-pip3 install . --no-build-isolation
+
+export MAX_JOBS=4
+export VERBOSE=1
+export NINJA_FLAGS="-v"
+export CFLAGS="-v" 
+export CXXFLAGS="-v"
+pip3 install . --no-build-isolation -vvv
 
 # pip3 install https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.2/flash_attn-2.8.2+cu12torch2.6cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
