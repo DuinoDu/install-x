44735d74c61e66f723a170f2fa9d6ea64e1d0efe
bingwen.ai <bingwen.ai@bytedance.com>
2025-11-26 10:29:25 +0800

    update executorch.sh

--- src/install_x/install/executorch.sh
+++ src/install_x/install/executorch.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# 
+# bash $INSTALL/executorch.sh       # install by pip
+# bash $INSTALL/executorch.sh 1     # install by source
 
 _curfile=$(realpath $0)
 cur=$(dirname $_curfile)
@@ -20,26 +23,26 @@ else
     pip3 install transformers
 fi
 
-install_from_pip=1
-if [ ! -n "$1" ];then
-    install_from_pip=0
+install_from_source=0
+if [ -n "$1" ];then
+    install_from_source=1
 fi
 
-if [[ $install_from_pip == "1" ]]; then
-    if python3 -c "import executorch" 2>/dev/null; then
-        version=$(python3 -c "from executorch import version; print(version.__version__)" 2>/dev/null || echo "Unknown")
-        echo "  executorch version: $version"
-    else
-        pip3 install executorch==1.0.1
-    fi
-else
-    pip3 uninstall executorch==1.0.1
+if [[ $install_from_source == "1" ]]; then
+    pip3 uninstall executorch -y
     OLD_GITHUB=$GITHUB
     GITHUB="."
     prepare_github https://github.com/pytorch/executorch.git 3.10 v1.0.1
     GITHUB=$OLD_GITHUB
     
     # TODO: add more scripts
+else
+    if python3 -c "import executorch" 2>/dev/null; then
+        version=$(python3 -c "from executorch import version; print(version.__version__)" 2>/dev/null || echo "Unknown")
+        echo "  executorch version: $version"
+    else
+        pip3 install executorch==1.0.1
+    fi
 fi
 
 if [ Darwin = 'Darwin' ]; then
